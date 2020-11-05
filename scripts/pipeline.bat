c79eb722da35368f8024643a1548410aac72faa4
macleginn <dnikolaev@fastmail.com>
2020-11-05 15:18:37 +0200

    New data for download; autogenerated patterns.

--- scripts/pipeline.bat
+++ scripts/pipeline.bat
@@ -5,4 +5,5 @@ python convert-structural-table.py
 python compute_language_stats.py
 :: Combines the meta provided by Say with computed stats.
 python collect_language_meta.py
+python prepare_data_for_download.py
 python generate.py -r
--- scripts/prepare_data_for_download.py
+++ scripts/prepare_data_for_download.py
@@ -11,3 +11,40 @@ for _, row in d_struct.iterrows():
 d_down = pd.DataFrame.from_records(records, columns=d_full.columns)
 del d_down['predicate_label_ru']
 d_down.to_csv('../data/data_for_download.csv', sep='\t', index=False)
+
+# Extract patterns
+d_meta = pd.read_csv('../data/languages.csv', sep='\t')
+lang_names = {
+    t.language_no: t.language for t in d_meta.itertuples()
+}
+d_pred_meta = pd.read_csv('../data/predicates.csv', sep='\t')
+predicate_info = {
+    t.predicate_no: {
+        'predicate_label_en': t.predicate_label_en,
+        'argument_frame_en':  t.argument_frame_en,
+        'predicate_label_ru': t.predicate_label_ru,
+        'argument_frame_ru':  t.argument_frame_ru
+    } for t in d_pred_meta.itertuples()
+}
+meta_colnames = ['predicate_no',
+                'predicate_label_en',
+                'argument_frame_en',
+                'predicate_label_ru',
+                'argument_frame_ru']
+column_names = meta_colnames + sorted(lang_names.values())
+patterns_df = pd.DataFrame(columns=column_names,
+                           index=sorted(predicate_info))
+
+# Fill in predicate info
+for k, v in predicate_info.items():
+    patterns_df.loc[k, 'predicate_no'] = k
+    for field in meta_colnames[1:]:
+        patterns_df.loc[k, field] = v[field]
+
+# Fill in valency patterns
+for t in d_down.itertuples():
+    language = lang_names[t.language_no]
+    predicate_no = t.predicate_no
+    patterns_df.loc[predicate_no, language] = t.valency_pattern
+
+patterns_df.to_csv('../data/patterns.csv', sep='\t', index=False)
